7743478765876980e876920ba9ae2508fc13e7e8
kohli <awais.mazher@freedommobile.ca>
2020-04-01 15:35:28 -0400

    styled components added

--- src/components/hello-world/index.jsx
+++ src/components/hello-world/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import style from './hello-world.css';
+import { Button } from './css';
 
 const HelloWorld = ({ title }) => {
-  return <div className={style['hello-world']}>{title}</div>;
+  return (
+    <div className={style['hello-world']}>
+      {title}
+      <Button>Hello Styled Components</Button>
+    </div>
+  );
 };
 
 HelloWorld.propTypes = {
